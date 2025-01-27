a66924ebd327cc3a7bdccdadfce576a1b6ef1c8c
Marius Lange <marius.lange@bsse.ethz.ch>
2025-01-27 16:30:42 +0100

    Add a relastic test case for the SampleAnnotator

--- tests/test_sample_annotator.py
+++ tests/test_sample_annotator.py
@@ -10,17 +10,36 @@ from numpy.random import binomial, negative_binomial
 from cell_annotator._response_formats import CellTypeMappingOutput, PredictedCellTypeOutput
 from cell_annotator.sample_annotator import SampleAnnotator
 
+# Declare the dictionary of expected marker genes
+expected_marker_genes = {
+    "Neuron": ["MAP2", "NEFL", "RBFOX3", "SYN1", "GAP43", "DCX", "TUBB3", "NEUROD1", "STMN2", "ENO2"],
+    "Fibroblast": ["COL1A1", "COL3A1", "VIM", "ACTA2", "FAP", "PDGFRA", "THY1", "FN1", "SPARC", "S100A4"],
+}
 
-def get_example_data() -> AnnData:
-    """Create example data for testing. Adapted from scanpy."""
-    adata = AnnData(np.multiply(binomial(1, 0.15, (100, 20)), negative_binomial(2, 0.25, (100, 20))))
-    # Adapt marker genes for cluster (so as to have some form of reasonable input)
-    adata.X[0:10, 0:5] = np.multiply(binomial(1, 0.9, (10, 5)), negative_binomial(1, 0.5, (10, 5)))
+
+def get_example_data(n_cells: int = 100) -> AnnData:
+    """Create example data for testing. Adapted from scanpy.
+
+    The data consists of two clusters with different marker genes. The first cluster is enriched for neuronal markers and the second cluster is enriched for fibroblast markers."""
+    gene_names = expected_marker_genes["Neuron"] + expected_marker_genes["Fibroblast"]
+    n_genes = len(gene_names)
+    adata = AnnData(np.multiply(binomial(1, 0.15, (n_cells, n_genes)), negative_binomial(2, 0.25, (n_cells, n_genes))))
+    adata.var_names = gene_names
+
+    # Create marker genes for the two clusters
+    n_group_1 = np.floor(0.3 * n_cells).astype(int)
+    n_group_2 = n_cells - n_group_1
+    n_marker_genes = int(n_genes / 2)
+
+    adata.X[:n_group_1, :10] = np.multiply(
+        binomial(1, 0.9, (n_group_1, n_marker_genes)), negative_binomial(1, 0.5, (n_group_1, n_marker_genes))
+    )
+    adata.X[n_group_1:, 10:] = np.multiply(
+        binomial(1, 0.9, (n_group_2, n_marker_genes)), negative_binomial(1, 0.5, (n_group_2, n_marker_genes))
+    )
 
     # Create cluster according to groups
-    adata.obs["leiden"] = pd.Categorical(
-        np.concatenate((np.zeros((10,), dtype=int), np.ones((90,), dtype=int))).astype(str)
-    )
+    adata.obs["leiden"] = pd.Categorical(np.concatenate((n_group_1 * ["0"], n_group_2 * ["1"])))
 
     # filter, normalize and log transform the data
     sc.pp.filter_cells(adata, min_counts=2)
@@ -44,7 +63,6 @@ class TestSampleAnnotator:
             stage="adult",
             cluster_key="leiden",
             model="gpt-4o-mini",
-            max_tokens=100,
         )
 
     @patch("cell_annotator.sample_annotator.SampleAnnotator.query_openai")
@@ -52,7 +70,10 @@ class TestSampleAnnotator:
         mock_response = PredictedCellTypeOutput(cell_type="Neuron")
         mock_query_openai.return_value = mock_response
 
-        sample_annotator.marker_genes = {"0": ["gene1", "gene2"], "1": ["gene3", "gene4"]}
+        sample_annotator.marker_genes = {
+            "0": expected_marker_genes["Neuron"][:2],
+            "1": expected_marker_genes["Fibroblast"][:2],
+        }
         sample_annotator.annotate_clusters(min_markers=1, expected_marker_genes=None)
 
         assert sample_annotator.annotation_dict["0"].cell_type == "Neuron"
@@ -72,12 +93,14 @@ class TestSampleAnnotator:
         assert all(sample_annotator.annotation_df["cell_type_harmonized"] == "Neuron")
 
     def test_get_cluster_markers(self, sample_annotator):
-        sample_annotator.get_cluster_markers(min_auc=0.5)
+        sample_annotator.get_cluster_markers(min_auc=0.6)
 
         assert sample_annotator.marker_gene_dfs is not None
         assert sample_annotator.marker_genes is not None
 
         for _cluster, df in sample_annotator.marker_gene_dfs.items():
+            print(f"Cluster {_cluster} Marker Genes:")
+            print(df)
             assert not df.empty
             assert "gene" in df.columns
             assert "specificity" in df.columns
@@ -85,3 +108,13 @@ class TestSampleAnnotator:
 
         for _cluster, genes in sample_annotator.marker_genes.items():
             assert len(genes) > 0
+
+    @pytest.mark.openai()
+    def test_annotate_clusters_actual(self, sample_annotator):
+        sample_annotator.get_cluster_markers(min_auc=0.6)
+        sample_annotator.annotate_clusters(min_markers=1, expected_marker_genes=expected_marker_genes)
+
+        print("Annotations:")
+        print(sample_annotator.annotation_df[["n_cells", "cell_type"]])
+        assert sample_annotator.annotation_dict["0"].cell_type == "Neuron"
+        assert sample_annotator.annotation_dict["1"].cell_type == "Fibroblast"
