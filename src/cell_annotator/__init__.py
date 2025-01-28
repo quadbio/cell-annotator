de7ee3433f3a37efa4a4d3cec5a06669c36f4e6e
Marius Lange <marius.lange@bsse.ethz.ch>
2025-01-28 18:39:16 +0100

    Draft first tutorial notebook

--- src/cell_annotator/__init__.py
+++ src/cell_annotator/__init__.py
@@ -5,5 +5,4 @@ from .cell_annotator import CellAnnotator
 from .sample_annotator import SampleAnnotator
 
 __all__ = ["CellAnnotator", "SampleAnnotator", "BaseAnnotator"]
-
 __version__ = version("cell-annotator")
