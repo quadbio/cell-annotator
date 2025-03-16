d3835d07a9d758d2bb33043e0de5ae6a830cb51a
Marius1311 <marius.lange@bsse.ethz.ch>
2025-03-16 19:12:29 +0100

    Expose the logger

--- src/cell_annotator/__init__.py
+++ src/cell_annotator/__init__.py
@@ -1,8 +1,9 @@
 from importlib.metadata import version
 
+from ._logging import logger
 from .base_annotator import BaseAnnotator
 from .cell_annotator import CellAnnotator
 from .sample_annotator import SampleAnnotator
 
-__all__ = ["CellAnnotator", "SampleAnnotator", "BaseAnnotator"]
+__all__ = ["CellAnnotator", "SampleAnnotator", "BaseAnnotator", "logger"]
 __version__ = version("cell-annotator")
